4820fae55fdd74773a8b9bdb689fa8a236a1f420
Artem Kuritsyn <94130870+tema4578@users.noreply.github.com>
2022-04-14 16:52:58 +0300

    переписка с картинками

--- main_app.py
+++ main_app.py
@@ -14,7 +14,7 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 @app.route("/")
 def index():
-    db_session.global_init("users2.db")
+    db_session.global_init("users3.db")
     db_sess = db_session.create_session()
     message = list(db_sess.query(Message).filter((Message.user_name.in_(['Пользователь 2', 'Пользователь 3'])), (Message.user_to_name.in_(['Пользователь 2', 'Пользователь 3']))))
     lst = []
@@ -33,11 +33,8 @@ def index():
             count += 1
         count2 += 1
 
-
-    #message = db_sess.query(Message).filter(Message.user_name == "Пользователь 2", Message.user_to_name == "Пользователь 3")
-                                            #(Message.user_name == "Пользователь 3", Message.user_to_name == "Пользователь 2"))
     return render_template("index2.html", message=reslst)
 
 
 if __name__ == '__main__':
-    app.run(port=8088, host='127.0.0.1')
+    app.run(port=8028, host='127.0.0.1')
